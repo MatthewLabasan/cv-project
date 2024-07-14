a6234cc710c5213c6a8f6fa5ef1589ddc241938c
MatthewLabasan <matthewjlabasan@gmail.com>
2024-07-14 02:43:42 -1000

    implemented basic ocr and added test images

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 1. Overview: capture feed (image / video) with OpenCV
 2. Convert frames to data/information with Tesseract & PyTesseract
 3. Output visual feedback (text) and store data
+
+# Requirement Notes
+1. Install tesseract (different on windows)
+2. Install pytesseract using pip
+3. Install opencv-python using pip
--- pythonOCR.py
+++ pythonOCR.py
@@ -1,7 +1,29 @@
-import cv2 # open CV module
-import pytesseract
+import cv2 # open cv module
+import pytesseract # ocr module
 
-# notice that as long as we change interpreter to conda, it will work
+# don't need line below as long as you set up a virtual env and reinstall requirements and tesseract. not sure how to do tesseract on windows yet!
 
-# don't need in downloaded program as long as you set up a virtual env and reinstall requirements
-pytesseract.pytesseract.tesseract_cmd = "/Users/matthewlabasan/anaconda3/lib/python3.11/site-packages/pytesseract"
+# use path to tesseract executable. to find this on mac: `which tesseract`. to find this on windows: `where tesseract``
+pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
+
+# load the frame
+img1 = cv2.imread('./test_images/sandIslandTest1_live.png')
+img2 = cv2.imread('./test_images/GUIcircles.png')
+img3 = cv2.imread('./test_images/GUI.png')
+
+# show the frame
+cv2.imshow('image 1', img1)
+cv2.imshow('image 2', img2)
+cv2.imshow('image 3', img3)
+
+cv2.waitKey(1) # 0: still img, 1: img for 1ms only, etc.
+
+# detect text on img
+print("img1: \n" + pytesseract.image_to_string(img1) + "\n")
+print("img2: \n" + pytesseract.image_to_string(img2) + "\n")
+print("img3: \n" + pytesseract.image_to_string(img3) + "\n")
+
+# tesseract problem: reading both black and white text?
+# cannot read the knots and direction number within block..
+# may need to do image enhancements / modifications for each frame
+# why can't it grab this bigger white numbers when all the others are the same? is there a way to subsection the image for text grabbing without creating new image files?
