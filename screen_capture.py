ee283b5237cf60d862e554f77a1fe208647a97a7
MatthewLabasan <matthewjlabasan@gmail.com>
2024-07-17 02:40:06 -1000

    fixed screen recorder resolution issues

--- screen_capture.py
+++ screen_capture.py
@@ -1,17 +1,28 @@
 import cv2
 from PIL import ImageGrab # from pillow
 import numpy as np
+from screeninfo import get_monitors # works well on windows, not macOS
 
-height = 1028
-width = 1028
+height = 0
+width = 0
+
+# get dimensions of screen
+for m in get_monitors():
+    width = m.width
+    height = m.height
+    print(m)
+
+screen_size = (width, height)
 
 # video encoding
-fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v") # choose a video type
-captured_video = cv2.VideoWriter("recorded_video.mp4", fourcc, 30.0, (height, width)) # title of video, format, fps, height and width of screen
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # choose a video type. * unpacks string into separate char args
+captured_video = cv2.VideoWriter("recorded_video.mp4", fourcc, 1.0, screen_size, True) # title of video, format, fps, width and height of screen, color
 
 while True:
     # modify last two numbers to simplify where the gui is captured
-    img = ImageGrab.grab(bbox = (0, 0, height, width)) # first two are top right corner, then height width
+    img = ImageGrab.grab(bbox = (0, 0, width, height)) # first two are top right corner, then height width
+
+    # img = ImageGrab.grab() # full screen -> use with mac testing, but should be fine in windows pc
 
     # put img into np array: holds rgb information for each pixel. form is (H, W, Channel)
     np_img = np.array(img)
@@ -25,9 +36,10 @@ while True:
     # write to video
     captured_video.write(cvt_img)
 
-    # waits for 20ms. if a key is pressed matching ascii 27 (esc key), loop breaks
-    key = cv2.waitKey(20)
+    # waits for 1s. if a key is pressed matching ascii 27 (esc key), loop breaks. otherwise, take next photo
+    key = cv2.waitKey(1000)
     if key == 27:
         break
 
+captured_video.release
 cv2.destroyAllWindows
